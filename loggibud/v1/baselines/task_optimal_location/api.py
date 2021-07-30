03570f27880f0a7c7123abff249c4b22175de4af
juliapp <aragaopintojuli@gmail.com>
2021-07-30 14:59:14 -0300

    refactor cors and run http method

--- loggibud/v1/baselines/task_optimal_location/api.py
+++ loggibud/v1/baselines/task_optimal_location/api.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, make_response
+from flask import Flask, request
+from flask_cors import CORS
 import json
+
 
 from loggibud.v1.baselines.task_optimal_location.utils.areas import getLoggiAreas
 
@@ -9,27 +11,20 @@ from loggibud.v1.baselines.task_optimal_location.utils.resolve_arg import (
   resolve_calc_method,
   resolve_K,
   resolve_algorithm,
-  resolve_solver_response
 )
 from loggibud.v1.baselines.task_optimal_location.utils.generator_factories import (
   instancesGeneratorFactory
 )
 
 app = Flask(__name__)
+CORS(app)
 
-def _build_cors_prelight_response(res):
-  response = make_response(res)
-  response.headers.add("Access-Control-Allow-Origin", "*")
-  response.headers.add('Access-Control-Allow-Headers', "*")
-  response.headers.add('Access-Control-Allow-Methods', "*")
-  return response
-
-@app.route('/run', methods=['GET'])
+@app.route('/run', methods=['POST'])
 def run():
-  body = request.get_json()
+  body = request.get_json()["data"]
   try:
-    paths = resolve_location_id('test')
-    # paths = resolve_location_id(body['location_id'])
+    # paths = resolve_location_id('test')
+    paths = resolve_location_id(body['location_id'])
 
     instances = instancesGeneratorFactory(paths)
 
@@ -43,19 +38,21 @@ def run():
 
     res = solver(instances, candidates, old, k)
 
-    return _build_cors_prelight_response(res)
+    response = res
 
   except ValueError as e:
-    return { "message": str(e) }, 422
+    response = ({ "message": str(e) }, 422)
+
+  return response
 
 
 @app.route('/get-algorithms', methods=['GET']) 
 def getAlgorithms():
-  return _build_cors_prelight_response(json.dumps(['minmax', 'minsum']))
+  return json.dumps(['minmax', 'minsum'])
 
 @app.route('/get-areas', methods=['GET']) 
 def getAreas():
-  return _build_cors_prelight_response(json.dumps(getLoggiAreas()))
+  return json.dumps(getLoggiAreas())
 
 
 app.run()
