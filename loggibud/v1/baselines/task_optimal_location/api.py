8d4949d516989bf77396e5aace146c66463008d6
Juliana <aragaopintojuli@gmail.com>
2021-09-23 13:54:58 -0300

    add procfile

--- loggibud/v1/baselines/task_optimal_location/api.py
+++ loggibud/v1/baselines/task_optimal_location/api.py
@@ -24,65 +24,66 @@ from loggibud.v1.baselines.task_optimal_location.utils.generator_factories impor
   instancesGeneratorFactory
 )
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+  app = Flask(__name__)
+  CORS(app)
 
-@app.route('/run', methods=['POST'])
-def run():
-  body = request.get_json()["data"]
-  try:
-    paths = resolve_location_id(body['location_id'])
+  @app.route('/run', methods=['POST'])
+  def run():
+    body = request.get_json()["data"]
+    try:
+      paths = resolve_location_id(body['location_id'])
 
-    instances = instancesGeneratorFactory(paths)
+      instances = instancesGeneratorFactory(paths)
 
-    (candidates, len_candidates) = resolve_candidates(body["candidates"])
+      (candidates, len_candidates) = resolve_candidates(body["candidates"])
 
-    old = resolve_calc_method("distance_matrix_great_circle")
+      old = resolve_calc_method("distance_matrix_great_circle")
 
-    k = resolve_K(body["k"], len_candidates)
+      k = resolve_K(body["k"], len_candidates)
 
-    solver = resolve_algorithm(body["algorithm"])
+      solver = resolve_algorithm(body["algorithm"])
 
-    res = solver(instances, candidates, old, k)
+      res = solver(instances, candidates, old, k)
 
-    response = res
+      response = res
 
-  except ValueError as e:
-    response = ({ "message": str(e) }, 422)
+    except ValueError as e:
+      response = ({ "message": str(e) }, 422)
 
-  return response
+    return response
 
-@app.route('/get-algorithms', methods=['GET']) 
-def getAlgorithms():
-  return json.dumps(['minmax', 'minsum'])
+  @app.route('/get-algorithms', methods=['GET']) 
+  def getAlgorithms():
+    return json.dumps(['minmax', 'minsum'])
 
-@app.route('/get-regions', methods=['GET']) 
-def getRegions():
-  return json.dumps(getLoggiRegions())
+  @app.route('/get-regions', methods=['GET']) 
+  def getRegions():
+    return json.dumps(getLoggiRegions())
 
-@app.route('/get-region-data', methods=['GET'])
-def getRegionData():
-  args = request.args.get
-  try:
-    paths = resolve_location_id(args('location_id'))
+  @app.route('/get-region-data', methods=['GET'])
+  def getRegionData():
+    args = request.args.get
+    try:
+      paths = resolve_location_id(args('location_id'))
 
-    percentage = resolve_region_data_percentage(args('percentage'))
+      percentage = resolve_region_data_percentage(args('percentage'))
 
-    region_generator = resolve_point_type(args('point_type'))
+      region_generator = resolve_point_type(args('point_type'))
 
-    data = region_generator(paths, percentage)
+      data = region_generator(paths, percentage)
 
-    compressed = compress(data)
-    
-    response = Response(compressed)
+      compressed = compress(data)
+      
+      response = Response(compressed)
 
-    response.headers['content-encoding'] = 'gzip'
-    response.headers['Content-Type'] = 'application/json'
-    
-  except ValueError as e:
-    response = ({ "message": str(e) }, 422)
+      response.headers['content-encoding'] = 'gzip'
+      response.headers['Content-Type'] = 'application/json'
+      
+    except ValueError as e:
+      response = ({ "message": str(e) }, 422)
 
-  return response
+    return response
 
-
-app.run()
+  return app
+  # app.run()
