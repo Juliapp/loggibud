50bac615b4319b914578db73e74eaaa049ad406c
Juliana <aragaopintojuli@gmail.com>
2021-10-23 12:22:25 -0300

    config specs

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -1,1 +1,5 @@
 # --log-level info --workers 3 --log-level=debug --timeout 21600 -b 0.0.0.0:5000 'loggibud.v1.baselines.task_optimal_location.api:create_app()'
+loglevel = 'info'
+workers =  3
+timeout = 21600
+bind = '0.0.0.0:443'
