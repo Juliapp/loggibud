3b22f2b1fc0b96d501b972db6e06663c0532aafb
Juliana <aragaopintojuli@gmail.com>
2021-10-24 20:52:45 -0300

    change port to ssl

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 loglevel = 'info'
 workers =  3
 timeout = 21600
-bind = '0.0.0.0:5000'
+bind = '0.0.0.0:443'
 certfile = '/home/ssl/loggiolq_certificate.pem'
 keyfile = '/home/ssl/loggiolq_key.pem'
